a0120f035d0b5ca88ed2eb27d227229b45b2df94
Marcin Gajda <marcin.gajda1978@hotmail.com>
2017-09-07 14:08:08 +0200

    mod 18

--- src/Store/configureStore.ts
+++ src/Store/configureStore.ts
@@ -1,38 +1,32 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
+import * as promise from 'redux-promise';
 import todoApp from '../Reducers/';
 
-const promise = (store: any) => (next: any) => (action: any) => {
-    if (typeof action.then === 'function') {
-        return action.then(next);
-    }
-    return next(action);
-};
+// const promise = (store: any) => (next: any) => (action: any) => {
+//     if (typeof action.then === 'function') {
+//         return action.then(next);
+//     }
+//     return next(action);
+// };
 
+// const logger = (store: any) => (next: any) => (action: any) => {
+//     console.group(action.type);
+//     console.log('prev state', store.getState());
+//     console.log('action', action);
+//     const returnValue = next(action);
+//     console.log('next state', store.getState());
+//     console.groupEnd();
+//     return returnValue;
 
-const logger = (store: any) => (next: any) => (action: any) => {
-    console.group(action.type);
-    console.log('prev state', store.getState());
-    console.log('action', action);
-    const returnValue = next(action);
-    console.log('next state', store.getState());
-    console.groupEnd();
-    return returnValue;
-
-};
-
-const wrapDispatchWithMidleware = (store: any, middlewares: any) => {
-    middlewares.forEach((element: any) => {
-        store.dispatch = element(store)(store.dispatch);
-    });
-};
+// };
 
 const configureStore = () => {
-    const store = createStore(todoApp);
     const middlewares = [promise];
     if (process.env.NODE_ENV !== 'production') {
         middlewares.push(logger);
     }
-    wrapDispatchWithMidleware(store, middlewares);
+    const store = createStore(todoApp, applyMiddleware(...middlewares));
     return store;
 };
 
