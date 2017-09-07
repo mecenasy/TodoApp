c94802f9a5cc747e0a53a0d6d5718adbedbb4aaa
Marcin Gajda <marcin.gajda1978@hotmail.com>
2017-09-07 09:22:18 +0200

    pop

--- src/Componet/AddTodo.tsx
+++ src/Componet/AddTodo.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { addTodo } from '../Action/';
+import { addTodo } from '../Action';
 class AddTodo extends React.Component<{ dispatch: any }, {}> {
 
     public render() {
