07b740f4637b1f715150d71d373baf6cf92519a6
Marcin Gajda <marcin.gajda1978@hotmail.com>
2017-09-08 08:13:40 +0200

    mod 19a

--- src/Store/configureStore.ts
+++ src/Store/configureStore.ts
@@ -3,24 +3,6 @@ import logger from 'redux-logger';
 import * as promise from 'redux-promise';
 import todoApp from '../Reducers/';
 
-// const promise = (store: any) => (next: any) => (action: any) => {
-//     if (typeof action.then === 'function') {
-//         return action.then(next);
-//     }
-//     return next(action);
-// };
-
-// const logger = (store: any) => (next: any) => (action: any) => {
-//     console.group(action.type);
-//     console.log('prev state', store.getState());
-//     console.log('action', action);
-//     const returnValue = next(action);
-//     console.log('next state', store.getState());
-//     console.groupEnd();
-//     return returnValue;
-
-// };
-
 const configureStore = () => {
     const middlewares = [promise];
     if (process.env.NODE_ENV !== 'production') {
