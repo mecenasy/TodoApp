0222c14c1b4858ddde8b5b97e29e70deda3c51b1
mecenasy <mecenasy@gmail.com>
2017-09-10 17:18:48 +0200

    12

--- src/Api/index.ts
+++ src/Api/index.ts
@@ -1,35 +1,52 @@
 import { v4 } from 'node-uuid';
 import { Todo } from '../Types/TodoStore';
 const fakeDatabase: Todo[] = [{
-    completed: true,
-    id: v4(),
-    text: 'hey',
+   completed: true,
+   id: v4(),
+   text: 'hey',
 }, {
-    completed: true,
-    id: v4(),
-    text: 'ho',
+   completed: true,
+   id: v4(),
+   text: 'ho',
 }, {
-    completed: false,
-    id: v4(),
-    text: 'let’s go',
+   completed: false,
+   id: v4(),
+   text: 'let’s go',
 }];
+
+export const addTodo = (text: string) =>
+   delay(500).then(() => {
+      const todo = {
+         completed: false,
+         id: v4(),
+         text,
+      };
+      fakeDatabase.push(todo);
+      return todo;
+   });
+
+export const toggleTodo = (id: any) =>
+   delay(500).then(() => {
+      const todo = fakeDatabase.find((t) => t.id === id);
+      if (todo !== undefined) {
+         todo.completed = !todo.completed;
+      }
+      return todo;
+   });
 
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export const fetchTodos = (filter: string) =>
-    delay(500).then(() => {
-        if (Math.random() < 0.5) {
-            throw new Error('Ups wyjebało się');
-        }
-        console.log(Math.random());
-        switch (filter) {
-            case 'all':
-                return fakeDatabase;
-            case 'completed':
-                return fakeDatabase.filter((t: Todo) => t.completed);
-            case 'active':
-                return fakeDatabase.filter((t: Todo) => !t.completed);
-            default:
-                 throw new Error(`Unknow filter: ${filter}`);
-        }
-    });
+   delay(500).then(() => {
+      console.log(Math.random());
+      switch (filter) {
+         case 'all':
+            return fakeDatabase;
+         case 'completed':
+            return fakeDatabase.filter((t: Todo) => t.completed);
+         case 'active':
+            return fakeDatabase.filter((t: Todo) => !t.completed);
+         default:
+            throw new Error(`Unknow filter: ${filter}`);
+      }
+   });
--- src/Store/configureStore.ts
+++ src/Store/configureStore.ts
@@ -3,38 +3,12 @@ import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { todos } from '../Reducers/';
 
-<<<<<<< HEAD
-const promise = (store: any) => (next: any) => (action: any) => {
-    if (typeof action.then === 'function') {
-        return action.then(next);
-    }
-    return next(action);
-};
-
-const logger = (store: any) => (next: any) => (action: any) => {
-    console.group(action.type);
-    console.log('prev state', store.getState());
-    console.log('action', action);
-    const returnValue = next(action);
-    console.log('next state', store.getState());
-    console.groupEnd();
-    return returnValue;
-
-};
-
-const wrapDispatchWithMidleware = (store: any, middlewares: any) => {
-    middlewares.slice().reverse().forEach((element: any) => {
-        store.dispatch = element(store)(store.dispatch);
-    });
-};
-=======
 // const thank = (store: any) => (next: any) => (action: any) => {
 //     console.log(store);
 //     // console.log(next);
 //     // console.log(action);
 //     typeof action === 'function' ? action(store.dispatrch) : next(action);
 // };
->>>>>>> ad897dab52098e80bd4965a679b58e395b13fe57
 
 const configureStore = () => {
     const middlewares = [thunk];
